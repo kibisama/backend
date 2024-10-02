30ee94eb507164edded47d576f6d6e34b57d5a55
unknown <kibisama9@gmail.com>
2024-10-03 01:13:44 +0900

    hello world

--- middlewares/inv/scan.js
+++ middlewares/inv/scan.js
@@ -12,7 +12,7 @@ const scan = async (req, res, next) => {
   }
   const _inputDate = dayjs(inputDate);
   const _exp = dayjs(exp, "YYMMDD");
-
+  const now = dayjs();
   let result = await Inventory.findOne({ gtin, sn });
   if (!result) {
     result = await Inventory.create({
@@ -24,56 +24,53 @@ const scan = async (req, res, next) => {
       console.log(e);
     });
   }
+  let arg;
 
   switch (mode) {
     case "Receive":
-      result = await Inventory.updateOne(
-        {
-          gtin,
-          sn,
-        },
-        { $set: { dateReceived: _inputDate, source } }
-      ).catch((e) => {
-        console.log(e);
-      });
-      break;
-    case "Fill":
-      if (!result.dateFilled) {
-        result = await Inventory.updateOne(
-          {
-            gtin,
-            sn,
-          },
-          { $set: { dateFilled: dayjs() } }
-        ).catch((e) => {
-          console.log(e);
+      arg = { $set: { dateReceived: _inputDate, source } };
+      const oneYearFromNow = now.add(1, "y");
+      if (_exp.isBefore(oneYearFromNow)) {
+        return res.send({
+          error: 1,
+          data: await Inventory.findOneAndUpdate(
+            {
+              gtin,
+              sn,
+            },
+            arg,
+            { new: true }
+          ).catch((e) => {
+            console.log(e);
+          }),
         });
       }
       break;
+    case "Fill":
+      if (result.dateFilled) {
+        return res.send(result);
+      }
+      arg = { $set: { dateFilled: now } };
+      break;
     case "Reverse":
-      result = await Inventory.updateOne(
-        {
-          gtin,
-          sn,
-        },
-        { $set: { dateReversed: dayjs() }, $unset: { dateFilled: 1 } }
-      ).catch((e) => {
-        console.log(e);
-      });
+      arg = { $set: { dateReversed: now }, $unset: { dateFilled: 1 } };
       break;
     case "Return":
-      result = await Inventory.updateOne(
-        {
-          gtin,
-          sn,
-        },
-        { $set: { dateReturned: dayjs() } }
-      ).catch((e) => {
-        console.log(e);
-      });
+      arg = { $set: { dateReturned: now } };
       break;
     default:
+      return res.sendStatus(500);
   }
+  result = await Inventory.findOneAndUpdate(
+    {
+      gtin,
+      sn,
+    },
+    arg,
+    { new: true }
+  ).catch((e) => {
+    console.log(e);
+  });
   return res.send(result);
 };
 
