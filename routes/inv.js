92be32fe88158af13cd386030ea435cea9b7c7c2
unknown <kibisama9@gmail.com>
2024-10-07 15:57:37 +0900

    hello world

--- routes/inv.js
+++ routes/inv.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const scan = require("../controllers/scan");
-const search = require("../controllers/getInv");
+const getInv = require("../controllers/getInv");
 
 router.get("/", getInv);
 router.post("/scan", scan);
