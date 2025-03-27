c98c4b19b434152bdf9e8298cc49049f9f25b9c6
unknown <kibisama9@gmail.com>
2025-03-26 21:13:23 -0700

    0.1.0

--- services/inv/dailyOrder.js
+++ services/inv/dailyOrder.js
@@ -5,8 +5,17 @@ const package = require("./package");
 
 /**
  * @typedef {dailyOrder.DailyOrder} DailyOrder
+ * @typedef {Parameters<dailyOrder["updateOne"]>["0"]} UpdateParam
  */
 
+/**
+ * @param {UpdateParam}
+ * @returns {UpdateParam}
+ */
+const createUpdateParam = (obj) => {
+  const updateParam = { $set: { lastUpdated: new Date() } };
+  return obj ? Object.assign(updateParam, obj) : updateParam;
+};
 /**
  * @returns {dayjs.Dayjs}
  */
@@ -29,6 +38,10 @@ const createDO = async (package) => {
     console.log(e);
   }
 };
+/**
+ * @param {package.Package} package
+ * @returns {Promise<DailyOrder|undefined>}
+ */
 const upsertDO = async (package) => {
   try {
     let _dailyOrder = await dailyOrder.findOne({
@@ -43,12 +56,35 @@ const upsertDO = async (package) => {
     console.log(e);
   }
 };
+/**
+ * @param {string} gtin
+ * @returns {Promise<[item.Item]|undefined>}
+ */
 const findFilledItems = async (gtin) => {
   try {
     return await item.find({
       gtin,
       dateFilled: { $gte: getToday() },
     });
+  } catch (e) {
+    console.log(e);
+  }
+};
+/**
+ * @param {DailyOrder} dO
+ * @param {string} gtin
+ * @returns {Promise<DailyOrder|undefined>}
+ */
+const updateFilledItems = async (dO, gtin) => {
+  try {
+    const items = await findFilledItems(gtin);
+    if (items?.length > 0) {
+      return await dailyOrder.findOneAndUpdate(
+        { _id: dO._id },
+        createUpdateParam({ $addToSet: { items: items.map((v) => v._id) } }),
+        { new: true }
+      );
+    }
   } catch (e) {
     console.log(e);
   }
