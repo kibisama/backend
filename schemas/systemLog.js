299df09d739d5f67a125225159dd4d2696c28b46
unknown <kibisama9@gmail.com>
2025-04-22 21:04:55 -0700

    0.1.0

---
+++ schemas/systemLog.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const systemLogSchema = new Schema({
+  createdAt: { type: Date, reqruied: true },
+  msg: { type: String, required: true },
+  type: { type: String, required: true },
+  active: { type: Boolean, required: true, default: true },
+  date: String,
+  status: String,
+});
+
+const model = mongoose.model("System Log", systemLogSchema);
+/**
+ * @typedef {Awaited<ReturnType<model["create"]>>[0]} SystemLog
+ */
+
+module.exports = model;
