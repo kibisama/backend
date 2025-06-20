1f4032b08915a3071c30783a16178437c837d492
unknown <kibisama9@gmail.com>
2025-06-20 00:10:49 -0700

    0.2.0

--- services/apps/settings.js
+++ services/apps/settings.js
@@ -1,3 +1,15 @@
+const preset = {
+  storeName: "El Camino Pharmacy ",
+  storeAddress: "10940 Victory Blvd.",
+  storeCity: "North Hollywood",
+  storeState: "CA",
+  storeZip: "91606",
+  storePhone: "(818) 763-4334",
+  storeFax: "(818) 763-4610",
+  storeEmail: "elcaminopharmacy@gmail.com",
+  storeManager: "Janice Chang",
+};
+
 const generatePreset = () => {
   // if a doc does not exist, create a preset
 };
