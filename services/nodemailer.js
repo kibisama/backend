c2553abc1ffc09d135998d39e39167eaf1abf7ae
이현구 <kibisama9@gamil.com>
2025-07-21 13:06:13 -0700

    0.2.0

---
+++ services/nodemailer.js
@@ -0,0 +1,16 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    type: "OAuth2",
+    user: process.env.MAILER_EMAIL,
+    clientId: process.env.MAILER_CLIENT_ID,
+    clientSecret: process.env.MAILER_CLIENT_SECRET,
+    refreshToken: process.env.MAILER_REFTKN,
+    accessToken: process.env.MAILER_ACCTKN,
+    expires: 1484314697598,
+  },
+});
