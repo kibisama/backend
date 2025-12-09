2cc4ea73ac434906fa4713ca93e4702efc9e0ca7
이현구 <kibisama9@gamil.com>
2025-12-09 15:15:26 -0800

    0.4.2

--- api/client/index.js
+++ api/client/index.js
@@ -2,7 +2,8 @@ const axios = require("axios");
 const client = axios.create();
 client.defaults.baseURL = process.env.CLIENT_ADDRESS;
 const jwt = require("jsonwebtoken");
-const generateToken = () => jwt.sign("", process.env.JWT_ACCESS_TOKEN_SECRET);
+const generateToken = () =>
+  jwt.sign({ sub: "api" }, process.env.JWT_ACCESS_TOKEN_SECRET);
 
 module.exports = {
   async refresh_cache_delivery(invoiceCode) {
